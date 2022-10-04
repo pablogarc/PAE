f1be0666917262982cbef4b2740fb84c6aa1d038
pablogarc <juan.garo29si@gmail.com>
2022-10-03 22:28:16 -0500

    Tarea 3 terminada

---
+++ Tarea_3/src/app/pages/noticias/noticia-details/noticia-details.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NoticiaDetailsComponent } from './noticia-details.component';
+
+describe('NoticiaDetailsComponent', () => {
+  let component: NoticiaDetailsComponent;
+  let fixture: ComponentFixture<NoticiaDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NoticiaDetailsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NoticiaDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
